1a5492432e4f5cfe9546c5796a8f64b39ac4f0d9
rosebellotaza <rosebellnarieotaza@gmail.com>
2024-10-12 00:42:46 +0800

    feat: updated applayout and views

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar
+        class="px-3"
+        :color="theme === 'light' ? 'green-darken-1' : 'green-darken-4'" border
+      >
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          variant="elevated"
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <slot name="content"></slot>
+        </v-container>
+      </v-main>
+
+      <v-footer :color="theme === 'light' ? 'green-darken-1' : 'green-darken-4'" elevation="24" border app>Copyright @2024. WasteWise</v-footer>
+    </v-app>
+  </v-responsive>
+</template>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,94 +1,56 @@
 <script setup>
-import { ref } from 'vue'
-
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+import AppLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar
-        class="px-3"
-        color="green-darken-4
-"
-      >
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-img src="/images/logo2.png" width="400px" class="mx-auto"></v-img>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-12">
+          <v-card class="mx-auto" elevation="24">
+            <v-card-title class="text-center">
+              <h3 class="font-weight-black">Waste Wise</h3>
+              <p>Register Form</p>
+            </v-card-title>
 
-        <v-btn
-          :prepend-icon="
-            theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
-          "
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field
+                  label="Firstname"
+                  variant="outlined"
+                ></v-text-field>
+                <v-text-field
+                  label="Lastname"
+                  variant="outlined"
+                ></v-text-field>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto pt-16">
-              <v-card
-                class="mx-auto"
-                prepend-icon="mdi-account-plus"
-                subtitle="Register"
-                elevation="24"
-              >
-                <template v-slot:title>
-                  <span class="font-weight-black">Waste Wise</span>
-                </template>
+                <v-text-field
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                ></v-text-field>
+                <v-text-field
+                  label="Password Confirmation"
+                  variant="outlined"
+                ></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field
-                      label="Firstname"
-                      variant="outlined"
-                    ></v-text-field>
-                    <v-text-field
-                      label="Lastname"
-                      variant="outlined"
-                    ></v-text-field>
-                    <v-text-field
-                      label="Email"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-btn class="mt-2" type="submit" block color="green-darken-3"
+                  >Register</v-btn
+                >
+              </v-form>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
-                    <v-text-field
-                      label="Password Confirmation"
-                      variant="outlined"
-                    ></v-text-field>
-
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                  </v-form>
-
-                  <v divider class="my-5">
-                    <h5 class="text-center">
-                      Already have account?
-                      <RouterLink to="login">Log in here.</RouterLink>
-                    </h5>
-                  </v>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer
-        color="green-darken-4
-"
-        app
-        >Copyright @2024. WasteWise</v-footer
-      >
-    </v-app>
-  </v-responsive>
+              <v divider class="my-5">
+                <h5 class="text-center">
+                  Already have account?
+                  <RouterLink to="login">Log in here.</RouterLink>
+                </h5>
+              </v>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
